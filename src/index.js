2e21920c98c1674ca88e5ada563fd5e5a9811cae
Leia Quesada <leiaquesada@gmail.com>
2021-03-07 14:27:50 -0800

    use reactDOM to render store prop

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import App from './App';
+import * as serviceWorker from './serviceWorker';const store = createStore(reducers, {}, applyMiddleware(reduxThunk));ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+serviceWorker.register();
