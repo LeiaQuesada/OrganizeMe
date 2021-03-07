9927a916babde45d79268de136dc9e5bb218a207
Leia Quesada <leiaquesada@gmail.com>
2021-03-07 14:19:14 -0800

    add reducer function to app

---
+++ src/reducers/data.js
@@ -0,0 +1,8 @@
+import {FETCH_TODOS} from '../actions/types';export default (state = {}, action) => {
+    switch(action.type) {
+      case FETCH_TODOS:
+        return action.payload;
+      default:
+        return state;
+    }
+  };
